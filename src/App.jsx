151be218bfbb9b64b912372cb153d56accfd7a60
Ubuntu <ubuntu@react>
2017-08-16 23:24:43 +0000

    ch 1  and 2 complete. on pg 42 passing data

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+const contentNode = document.getElementById('contents');
+
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div> This is a placeholder for {'<'}IssueFilter{'>'} </div>
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    return (
+      <div> This is a placeholder for {'<'}IssueTable{'>'} </div>
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div> This is a placeholder for {'<'}IssueAdd{'>'} </div>
+    );
+  }
+}
+
+class IssueList extends React.Component {
+  render() { 
+    return(
+      <div>
+        <h1> Issue Tracker </h1>
+
+        <IssueFilter />
+        <hr />
+
+        <IssueTable />
+        <hr />
+
+        <IssueAdd />
+      </div>
+    ); 
+  }
+}
+ReactDOM.render(<IssueList />, contentNode);
