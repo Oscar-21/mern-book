16cc932792f54a4775cdaa477ce171a3eb99be22
Oscar-21 <austin.conder@outlook.com>
2017-08-22 01:42:47 -0400

    git

--- mern.js
+++ mern.js
@@ -10,22 +10,14 @@ const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
-const issues = [
-  {
-    id: 1, status: 'Open', owner: 'Ravan',
-    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-    title: 'Error in console when clicking Add',
-  },
-  {
-    id: 2, status: 'Assigned', owner: 'Eddie',
-    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
-    title: 'Missing bottom border on panel',
-  },
-];
 
 app.get('/api/issues', (req, res) => {
-  const metadata = { total_count: issues.length };
-  res.json({ _metadata: metadata, records: issues });
+  db.collection('issues').find().toArray().then(issues => {
+    const metadata = {total_count: issues.length};
+    res.json({ _metadata: metadata, records: issues });
+  }).catch(err => {
+    console.log('error: ', err);
+  });
 });
 
 const validIssueStatus = {
@@ -80,8 +72,7 @@ app.post('/api/issues', (req, res) => {
   res.json(newIssue);
 });
 
-MongoClient.connect('mongodb://localhost/issuetracker'
-).then(connection => {
+MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
   db = connection;
   app.listen(3000, () => {
     console.log('App started on port 3000');
